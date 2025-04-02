601ff5f103483cb4b2c963de7575fd1059a07ce5
Karttikeya Sinha <biscuitrescue>
2025-04-02 23:25:55 +0530

    swaylock-effects + upgrade

--- config/nvim/nvim.nix
+++ config/nvim/nvim.nix
@@ -1,16 +1,16 @@
-{ pkgs, lib, ... }:
-
-{
-  programs.nvf = {
-    enable = true;
-    # your settings need to go into the settings attribute set
-    # most settings are documented in the appendix
-    settings = {
-      vim.viAlias = false;
-      vim.vimAlias = true;
-      vim.lsp = {
-        enable = true;
-      };
-    };
-  };
-}
+# { pkgs, lib, ... }:
+#
+# {
+#   programs.nvf = {
+#     enable = true;
+#     # your settings need to go into the settings attribute set
+#     # most settings are documented in the appendix
+#     settings = {
+#       vim.viAlias = false;
+#       vim.vimAlias = true;
+#       vim.lsp = {
+#         enable = true;
+#       };
+#     };
+#   };
+# }
